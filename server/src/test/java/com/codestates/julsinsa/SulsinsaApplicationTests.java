d67ab2d41d33297e82cede7ffe968cdeb336c039
taegyun0903 <120002507+taegyun0903@users.noreply.github.com>
2023-05-18 23:53:34 +0900

    test 오류코드 삭제, appspec.yml, buildspec.yml 파일 위치이동, yml 파일 수정

--- server/src/test/java/com/codestates/julsinsa/SulsinsaApplicationTests.java
+++ server/src/test/java/com/codestates/julsinsa/SulsinsaApplicationTests.java
@@ -1,13 +1,8 @@
 package com.codestates.julsinsa;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SulsinsaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
